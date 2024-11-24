1d1ea92ae6d7da5201ef8e7db16a168bf7e3e1b1
Rashmika Perera <144840582+Rashmika20041@users.noreply.github.com>
2024-11-24 16:44:11 +0530

    fourth commit
    change path

--- Home/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Web Developer", "Designer", "Software Engineer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
